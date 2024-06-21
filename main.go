9ac94037608db6830b20169fdc5dc8ad2f80efd7
kc <151539784+33kc@users.noreply.github.com>
2024-06-21 23:02:01 +0530

    added cli flagging

--- main.go
+++ main.go
@@ -1,23 +1,74 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
 
-    "github.com/skip2/go-qrcode"
+	qrcode "github.com/skip2/go-qrcode"
 )
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter website URL: ")
-    url, _ := reader.ReadString('\n')
-    url = url[:len(url)-1] // Remove the newline character
+	var urlInput string
+	flag.StringVar(&urlInput, "url", "", "URL to encode as QR code")
+	flag.Parse()
 
-    err := qrcode.WriteFile(url, qrcode.Medium, 256, "qrcode.png")
-    if err != nil {
-        fmt.Println("Error generating QR code:", err)
-    } else {
-        fmt.Println("QR code generated and saved as qrcode.png")
-    }
+	if urlInput == "" {
+		fmt.Println("Error: Please provide a URL to encode as QR code")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+
+	_, err := url.ParseRequestURI(urlInput)
+	if err != nil {
+		fmt.Println("Error: Invalid URL format provided")
+		os.Exit(1)
+	}
+
+	qrCode, err := qrcode.Encode(urlInput, qrcode.Medium, 256)
+	if err != nil {
+		log.Fatal("Error generating QR code:", err)
+	}
+
+
+	fileName := "qrcode.png"
+
+
+	currentDir, err := getCurrentDirectory()
+	if err != nil {
+		log.Fatal("Error getting current directory:", err)
+	}
+	filePath := filepath.Join(currentDir, fileName)
+	err = saveQRCode(filePath, qrCode)
+	if err != nil {
+		log.Fatal("Error saving QR code:", err)
+	}
+
+	fmt.Printf("QR code saved as %s\n", filePath)
+}
+func saveQRCode(filePath string, qrCode []byte) error {
+	// Create a new file for writing
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(qrCode)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+func getCurrentDirectory() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return dir, nil
 }
